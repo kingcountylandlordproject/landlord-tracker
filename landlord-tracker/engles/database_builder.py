966f5a95fce72660c8cc04280a4662070b159d76
Thomas Jones <thomasbjones2@gmail.com>
2022-09-14 00:51:09 -0700

    adding land value extraction and common.py

--- landlord-tracker/engles/database_builder.py
+++ landlord-tracker/engles/database_builder.py
@@ -1,28 +1,8 @@
 import pandas as pd
-import yaml
 
 from sqlalchemy import create_engine 
+from common import drop_database_sql, drop_user_sql, get_configs, create_user_sql, create_database_sql
 
-CONFIG_PATH = "./config/postgres_db_config.yaml"
-
-
-CREATE_USER_SQL = 'CREATE USER :username WITH PASSWORD :passwd'
-
-def create_user_sql(username, passwd):
-    return f'CREATE USER {username} WITH PASSWORD \'{passwd}\''
-
-def create_database_sql(username, database):
-    return f'CREATE DATABASE {database} WITH OWNER = {username};'
-
-
-def get_configs():
-    with open(CONFIG_PATH, "r") as config:
-        try:
-            config = yaml.safe_load(config)
-        except yaml.YAMLError as exc:
-            print(exc)
-            exit()
-    return config
 
 def create_databases_from_config(config):
     user = config['user']
@@ -38,6 +18,16 @@ def create_databases_from_config(config):
     engine = create_engine(f'postgresql+psycopg2://{user}:{user_pw}@{hostname}')
     
     with engine.connect() as conn:
+        conn.execute("commit")
+        try:
+            conn.execute(drop_database_sql(database_name))
+        except Exception as e:
+            print(f"Warning, faluire to drop database {database_name}")
+        try:
+            conn.execute(drop_user_sql(table_user))
+        except Exception as e:
+            print(f"Warning, faluire to drop user {table_user}")
+        conn.execute("commit")
         conn.execute(create_user_sql(table_user, table_pw))
         conn.execute("commit")
         conn.execute(create_database_sql(table_user, database_name))
@@ -48,9 +38,10 @@ def create_databases_from_config(config):
         print(f"uploading table key: {key}")
         table = tables[key]
         raw_path = table['raw_path']
+        table_name = table['table_name']
         df = pd.read_csv(f"{data_path}{raw_path}", encoding='latin-1')
         print(df.head())
-        df.to_sql(key, engine)
+        df.to_sql(table_name, engine)
 
 def main():
     config = get_configs()
